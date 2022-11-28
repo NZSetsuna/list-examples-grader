61c06e12bce9d3f7ef3d27c0ff9c889bed36ab2e
NZSetsuna <zhz133@ucsd.edu>
2022-11-28 04:18:50 -0800

    fix

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,65 @@
 import static org.junit.Assert.*;
+import java.util.ArrayList;
+import java.util.List;
 import org.junit.*;
+
+class checkA implements StringChecker {
+  public boolean checkString(String s) {
+    if(s.contains("A")){
+      return true;
+    }  
+    return false;
+  }
+}
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+  public void testFilter(){
+    StringChecker sc = new checkA();
+
+    List<String> input1 = new ArrayList<>();
+    List<String> result = new ArrayList<>();
+
+    input1.add("TOYOTA");
+    input1.add("HONDA");
+    input1.add("NISSAN");
+    input1.add("MITSUBISHI");
+
+    result.add("TOYOTA");
+    result.add("HONDA");
+    result.add("NISSAN");
+
+    //assertArrayEquals(result.toArray(), ListExamples.filter(input1, sc).toArray());
+    assertEquals(result, ListExamples.filter(input1, sc));
+  }
+
+  @Test
+  public void testMerge(){
+    List<String> input1 = new ArrayList<>();
+    List<String> input2 = new ArrayList<>();
+    List<String> result = new ArrayList<>();
+
+    input1.add("ALPHA");
+    input1.add("CHARLIE");
+    input1.add("ECHO");
+    input1.add("GOLF");
+
+    input2.add("BRAVO");
+    input2.add("DELTA");
+    input2.add("FOXTROT");
+    input2.add("HOTEL");
+
+    result.add("ALPHA");
+    result.add("BRAVO");
+    result.add("CHARLIE");
+    result.add("DELTA");
+    result.add("ECHO");
+    result.add("FOXTROT");
+    result.add("GOLF");
+    result.add("HOTEL");
+    
+    assertEquals(result, ListExamples.merge(input1, input2));
+  }
+
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,46 @@
 # Create your grading script here
 
-set -e
+# set -e
 
 rm -rf student-submission
 git clone $1 student-submission
+
+echo "Successfully cloned"
+
+cd student-submission
+if [ -f ListExamples.java ]
+then
+    echo "File found"
+else
+    echo "File not found"
+    echo "Student's grade is 0"
+    exit 1
+fi
+
+cd ..
+cp -r lib student-submission/
+cp TestListExamples.java student-submission/
+cd student-submission
+
+javac -cp ".;lib/hamcrest-core-1.3.jar;lib/junit-4.13.2.jar" *.java
+
+if [ $? -eq 00 ]
+then
+    echo "Compiled"
+else
+    echo "Compilation failed"
+    echo "Student's grade is 0"
+    exit 1
+fi
+
+java -cp ".;lib/junit-4.13.2.jar;lib/hamcrest-core-1.3.jar" org.junit.runner.JUnitCore TestListExamples
+
+if [ $? -eq 00 ]
+then
+    echo "Test passed!"
+    echo "Student's grade is 2"
+else
+    echo "Test failed"
+    echo "Student's grade is 0"
+fi
 
